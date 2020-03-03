1f01c4fd5a2117b2c1eb702e4056858ed0405888
Eloi DUWER <eloitrocy@hotmail.fr>
2020-03-03 18:48:37 +0100

    petite opti + norme

--- srcs/ft_printf/parsing/precision_field_width.c
+++ srcs/ft_printf/parsing/precision_field_width.c
@@ -6,7 +6,7 @@
 /*   By: eduwer <eduwer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/22 16:31:44 by eduwer            #+#    #+#             */
-/*   Updated: 2020/03/03 18:21:32 by eduwer           ###   ########.fr       */
+/*   Updated: 2020/03/03 18:47:49 by eduwer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 static int		get_star_arg(t_printf_context *ctx)
 {
+	ctx->in_i++;
 	return ((int)va_arg(*(ctx->list), int));
 }
 
@@ -27,10 +28,7 @@ t_printf_state	get_field_width(t_printf_context *ctx)
 			ctx->in_i++;
 	}
 	else if (ctx->input[ctx->in_i] == '*')
-	{
 		ctx->field_width = get_star_arg(ctx);
-		ctx->in_i++;
-	}
 	return (get_precision(ctx));
 }
 
@@ -40,11 +38,9 @@ t_printf_state	get_precision(t_printf_context *ctx)
 	{
 		ctx->in_i++;
 		if (ctx->input[ctx->in_i] == '*')
-		{
 			ctx->precision = get_star_arg(ctx);
-			ctx->in_i++;
-		}
-		else {
+		else
+		{
 			ctx->precision = ft_atoi(&(ctx->input[ctx->in_i]));
 			while (ctx->input[ctx->in_i] != '\0' && \
 				ft_isdigit(ctx->input[ctx->in_i]) == 1)
