1012300ecd7fb4ea3ca17d5d2336211088f7793f
eduwer <eloitrocy@hotmail.fr>
2021-05-14 21:56:12 +0200

    fix off-by-one error in strdup

--- srcs/libft/ft_strdup.c
+++ srcs/libft/ft_strdup.c
@@ -6,31 +6,24 @@
 /*   By: eduwer <eduwer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/02 21:40:32 by eduwer            #+#    #+#             */
-/*   Updated: 2016/11/05 14:50:19 by eduwer           ###   ########.fr       */
+/*   Updated: 2021/05/14 18:07:19 by eduwer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <string.h>
+#include <libft.h>
 
 char		*ft_strdup(char *src)
 {
-	int		nb_char;
-	int		i;
+	size_t	src_len;
 	char	*newstr;
 
-	nb_char = 0;
-	while (src[nb_char] != '\0')
-		nb_char++;
-	newstr = malloc(sizeof(char) * (nb_char + 1));
+	src_len = ft_strlen(src);
+	newstr = malloc(sizeof(char) * (src_len + 1));
 	if (newstr == NULL)
 		return (newstr);
-	i = 0;
-	while (i <= nb_char)
-	{
-		newstr[i] = src[i];
-		i++;
-	}
-	newstr[i] = '\0';
+	ft_memcpy(newstr, src, src_len);
+	newstr[src_len] = '\0';
 	return (newstr);
 }
